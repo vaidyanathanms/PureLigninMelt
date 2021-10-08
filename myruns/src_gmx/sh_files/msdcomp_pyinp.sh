e329267c47a3a3e79b9ad33e174a1b2fe9b77094
vaidyanathanms_cades <msvaidyanathan1729@gmail.com>
2021-10-08 17:45:50 -0400

    runs on 100ps dt trajectories

--- myruns/src_gmx/sh_files/msdcomp_pyinp.sh
+++ myruns/src_gmx/sh_files/msdcomp_pyinp.sh
@@ -11,9 +11,10 @@
 #SBATCH -o outdir/out.%J
 #SBATCH -e outdir/err.%J
 
-module load gromacs
+module load PE-gnu/3.0
+module load cuda/10.1
+module load gromacs/2020.6
 
-export GMX_MAXBACKUP=-1;
 export OMP_NUM_THREADS=24;
 
 echo "begin job.."
@@ -23,39 +24,38 @@ echo $PWD
 # Inputs
 msdout="msd_nptmain"; outdir="msdanalysis"
 nchains=py_nchains
-
-if ! test -f "py_trajfile"; then
-    printf "py_trajfile not found"
-    exit 1
-elif ! test -f "py_tprfile"; then
-    printf "py_tprfile not found"
-    exit 1
-elif ! test -f "py_conffile"; then
-    printf "initconf not found"
-    exit 1
-elif ! test -f "chaininp.inp"; then
+	
+# Make Index files
+if ! test -f "chaininp.inp"; then
     echo "chaininp.inp not found"
     exit 1
 fi
-	
 
-# Make Index files
 if ! test -f "chindx.ndx"; then 
     srun gmx select -f py_conffile -s py_tprfile -sf chaininp.inp -on chindx.ndx
 fi
 wait
 
-# Compute Rg of chains
-printf "Computing Rg of chains"
-
-if [ ! -d ${outdir} ]; then
-    mkdir -p ${outdir}
+# Search for tpr/trr files
+if ! test -f "enermin.tpr"  && ! test -f "traj_npt_main.trr"; then
+    printf "tpr/trr files not found"
+    exit 1
 fi
+
+if ! test -f "traj_npt_main_nojump_100ps.trr"; then
+    printf "0" | srun gmx trjconv -s enermin.tpr -f traj_npt_main.trr -dt 100 -pbc nojump -o traj_npt_main_nojump_100ps.trr
 wait
+fi
+
+
+# Compute MSD of chains
+printf "Computing MSD of chains"
+
+mkdir -p ${outdir}
 
 for (( chcnt_i = 0; chcnt_i < nchains-1; chcnt_i++ ))
 do
-    printf "${chcnt_i}" | srun gmx msd -f py_trajfile -s py_tprfile -n chindx.ndx -o ${msdout}_${chcnt_i}.xvg &
+    printf "${chcnt_i}" | srun gmx msd -f traj_npt_main_nojump_100ps.trr -s py_tprfile -n chindx.ndx -o ${msdout}_${chcnt_i}.xvg &
 done
 wait
 
@@ -63,4 +63,4 @@ mv ${msdout}_*.xvg ${outdir}
 cp chainlist.dat ${outdir}
 cp chindx.ndx ${outdir}
 
-printf "End of Rg calculations.."
+printf "End of MSD calculations.."
--- myruns/src_gmx/sh_files/rgcomp_pyinp.sh
+++ myruns/src_gmx/sh_files/rgcomp_pyinp.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -A bsd
 #SBATCH -p batch
-#SBATCH -t 0-02:30:00
+#SBATCH -t 0-10:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
@@ -26,38 +26,37 @@ echo $PWD
 rgout="rg_nptmain"; outdir="rganalysis"
 nchains=py_nchains
 
-if ! test -f "py_trajfile"; then
-    printf "py_trajfile not found"
-    exit 1
-elif ! test -f "py_tprfile"; then
-    printf "py_tprfile not found"
-    exit 1
-elif ! test -f "py_conffile"; then
-    printf "initconf not found"
-    exit 1
-elif ! test -f "chaininp.inp"; then
+# Make Index files
+if ! test -f "chaininp.inp"; then
     echo "chaininp.inp not found"
     exit 1
 fi
-	
 
-# Make Index files
 if ! test -f "chindx.ndx"; then 
     srun gmx select -f py_conffile -s py_tprfile -sf chaininp.inp -on chindx.ndx
 fi
 wait
 
+# Search for tpr/trr files
+if ! test -f "enermin.tpr"  && ! test -f "traj_npt_main.trr"; then
+    printf "tpr/trr files not found"
+    exit 1
+fi
+
+if ! test -f "traj_npt_main_nojump_100ps.trr"; then
+    printf "0" | srun gmx trjconv -s enermin.tpr -f traj_npt_main.trr -dt 100 -pbc nojump -o traj_npt_main_nojump_100ps.trr
+wait
+fi
+
 # Compute Rg of chains
 printf "Computing Rg of chains"
 
-if [ ! -d ${outdir} ]; then
-    mkdir -p ${outdir}
-fi
+mkdir -p ${outdir}
 wait
 
 for (( chcnt_i = 0; chcnt_i <= nchains-1; chcnt_i++ ))
 do
-    printf "${chcnt_i}" | srun gmx gyrate -f py_trajfile -s py_tprfile -n chindx.ndx -o ${rgout}_${chcnt_i}.xvg &
+    printf "${chcnt_i}" | srun gmx gyrate -f traj_npt_main_nojump_100ps.trr -s py_tprfile -n chindx.ndx -o ${rgout}_${chcnt_i}.xvg &
 done
 wait
 
