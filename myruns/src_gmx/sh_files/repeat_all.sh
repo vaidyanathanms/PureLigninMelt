941693c272ef7cc560a753bbbd30c1ecd7e79641
vaidyanathanms_cades <msvaidyanathan1729@gmail.com>
2021-10-19 16:52:01 -0400

    updated sh files

--- myruns/src_gmx/sh_files/repeat_all.sh
+++ myruns/src_gmx/sh_files/repeat_all.sh
@@ -1,14 +1,10 @@
 #!/bin/bash 
 
 # submit the preprocessing job (run_preprocess.sh)
-ID=$(bsub run_preprocess.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
-
+ID=$(sbatch run_preprocess.sh | awk '{print $4}')
+echo "$ID"
 # submit the MD run job in a loop (run_md.sh)
-<<<<<<< HEAD
-for ((i=1;i<=3;i+=1))
-=======
-for ((i=1;i<=2;i+=1))
->>>>>>> origin/master
-do 
-ID=$(bsub -w "ended("$ID")" run_md.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
-done
+#for ((i=1;i<=3;i+=1))
+#do 
+#ID=$(bsub -w "ended("$ID")" run_md.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
+#done
--- myruns/src_gmx/sh_files/repeat_md.sh
+++ myruns/src_gmx/sh_files/repeat_md.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 # submit the run_md job (run_md.sh)
-ID=$(bsub run_md.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
-
+ID=$(sbatch run_md.sh | awk '{print $4}')
+echo "$ID"
 # submit the MD run job in a loop (run_md.sh)
-for ((i=1;i<=3;i+=1))
-do 
-ID=$(bsub -w "ended("$ID")" run_md.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
-done
+#for ((i=1;i<=3;i+=1))
+#do 
+#ID=$(sbatch -w "ended("$ID")" run_md.sh | awk '{print $2}' | tr "<" " " | tr ">" " " | awk '{print $1}')
+#done
