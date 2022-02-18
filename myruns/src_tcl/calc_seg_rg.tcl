37ada78edb27bf09645132ed1bd41fc523ffd0e4
vaidyanathanms <v0e@ornl.gov>
2022-02-17 19:36:25 -0500

    changed segname based selection to fragment based selection to be consistent with ParmEd inputs

--- myruns/src_tcl/calc_seg_rg.tcl
+++ myruns/src_tcl/calc_seg_rg.tcl
@@ -1,13 +1,13 @@
 # Compute segmental Rg 
 mol load psf ../init_files/L.psf ;# you can also use a PDB
 
-set ferseg "FERUT"
+set ferseg "FERU"
 set pcaseg "PCA"
 set psfval [atomselect top "all"]
-set seg [ lsort -unique [ $psfval get segname ] ]
+set seg [ lsort -unique [ $psfval get fragment ] ]
 set resall [ lsort -unique [ $psfval get resname ] ]
 
-mol addfile traj_npt_main_nojump_100ps.trr waitfor all 
+mol addfile traj_npt_main_nojump_100ps.trr first 2000 waitfor all  
 set num_frames [ molinfo top  get numframes ]
 set num_min    0 
 
@@ -21,7 +21,7 @@ puts $frg "segmentID \t n \t m \t m-n \t Rg"
 set Ntotmax 0
 # Main code
 foreach s $seg {
-    set res0 [lsort -unique [[atomselect top "segname $s and not resname $ferseg and not resname $pcaseg"] get resid]]
+    set res0 [lsort -unique [[atomselect top "fragment $s and not resname $ferseg and not resname $pcaseg"] get resid]]
     puts "old residue order: $res0"
     set res [ lsort -integer $res0 ]
     puts "updated res order: $res"
@@ -36,7 +36,7 @@ foreach s $seg {
 	    set nval [ lindex $res $n ]
 	    set mval [ lindex $res $m ]
 	    puts "nval: $nval mval: $mval"
-	    set sel [atomselect top "resid $nval to $mval and segname $s and not resname $ferseg and not resname $pcaseg" ]
+	    set sel [atomselect top "resid $nval to $mval and fragment $s and not resname $ferseg and not resname $pcaseg" ]
 	    set separation [  expr abs ( $m - $n) ]
 	    for {set i $num_min } { $i <= $num_frames-1  } { incr i } {
 		$sel frame $i
@@ -55,9 +55,9 @@ foreach s $seg {
 	    unset separation 
 	    unset sel
 	} ;# end for {set m [expr $n+1]}...
-	puts "finished segment [expr $n+1] of a total of [expr $Ntot-1] in chain $s"
+	puts "finished fragment [expr $n+1] of a total of [expr $Ntot-1] in chain $s"
     } ;#end for {set n 1}...
-    puts "finished all [expr $Ntot-1] Rg segments of chain $s"
+    puts "finished all [expr $Ntot-1] Rg fragments of chain $s"
     unset res
     unset Ntot
 } ;#end foreach seg
