ffed6f264e80879b2622bf7a83ec285d8ec31d29
vaidyanathanms <v0e@ornl.gov>
2022-03-16 16:12:11 -0400

    variable change and deleted dens calc

--- myruns/src_py/analyze_dens.py
+++ myruns/src_py/analyze_dens.py
@@ -30,117 +30,94 @@ nchains   = 20
 # Global arrays
 denarr = np.arange(temp_min,temp_max,5*temp_dt) #plotting time data
 #------------------------------------------------------------------
-# Compute densities for all systems
-if den_cal != 0:
-    print("Computing densities")
-    if not os.path.isdir(sh_dir):
-        raise RuntimeError(sh_dir + " does not exist!")
-    for bio_indx in range(len(biom_arr)): # loop in biomass
-        biomass = biom_arr[bio_indx]
+# Plot glass transition for all systems
+print("Analyzing Tg data")
+for bio_indx in range(len(biom_arr)): # loop in biomass
+    
+    biomass = biom_arr[bio_indx]
+    res_dir = scr_dir + '/' + inp_type + '/'+ biomass +\
+              '/pdi_' + str(pdi_val) + '/results'
+    if not os.path.isdir(res_dir):
+        os.mkdir(res_dir)
+        
+    # Write data and then read to concatenate
+    tg_fyl = res_dir + '/tgdata.dat'
+    fc_tg = open(tg_fyl,'w')
+    fc_tg.write('%s\t%s\t%s\n' %('Biomass','Temp','SV_NPT'))
+    
+    tg_avg = np.zeros(0)
+
+    # Define axes labels for distribution
+    fig1,ax1 = plt.subplots()
+    set_axes(ax1,plt,r'Time (ps)',r'Density ($kg$/$m^3$)')
+    
+    yrho_avg = np.zeros(0) # Average density
+
+    for tval in range(temp_min,temp_max,temp_dt): # loop in temp
+        temp_leg  = str(tval)
+        yall = np.zeros(0)
+        yavg = 0
+        
         for casenum in range(len(run_arr)): # loop in runarr
             wdir,tdir,fig_dir = ret_temp_dir(scr_dir,inp_type,biomass,\
                                              pdi_val,run_arr[casenum],\
-                                             600,solv_type)
-            print(wdir)
-            gencpy(sh_dir,wdir,'comp_dens_pyinp.sh')
-            submit_dens(os.getcwd(),wdir,'comp_dens_pyinp.sh',\
-                        temp_min,temp_max,temp_dt)
-#------------------------------------------------------------------
-# Plot glass transition for all systems
-if tg_cal != 0:
-    print("Analyzing Tg data")
+                                             tval,solv_type)
+            print("Analyzing: ", pdi_val,tval,run_arr[casenum])
+            # Check file
+            if os.path.exists(tdir + '/dens_npt.xvg'):
+                fname  = tdir + '/dens_npt.xvg'
+            elif os.path.exists(wdir + '/dens_npt.xvg'):
+                fname  = wdir + '/dens_npt.xvg'
+            else:
+                print(fname, " does not exist! ")
+                print("ERR: Compute densities before Tg calculation..")
+                continue
+                
+            # Open and parse file
+            with open(fname) as fin:
+                lines = (line.lstrip() for line in fin \
+                         if not line.lstrip().startswith('#') and \
+                         not line.lstrip().startswith('@'))
+                data  = np.loadtxt(lines)
 
-    for bio_indx in range(len(biom_arr)): # loop in biomass
+            yall = np.append(yall,data[:,1]) #append all y-data
+            startpt = int(0.1*len(yall))
+            yavg += np.average(data[startpt:len(yall)-1,1])
+            
+            if casenum == 0 and tval in denarr: # plot time-density variations
+                ax1.plot(data[:,0],data[:,1],label=temp_leg)
+            
+        # append avg to yrho_avg
+        yrho_avg = np.append(ytemp_avg,yavg/len(run_arr))
+        
+    # Save density-time plots
+    ax1.legend(loc=0)
+    fig1.savefig(fig_dir + '/'+'denstime_'+str(pdi_val)+'.png',\
+                 dpi=fig1.dpi)
+    plt.close(fig1)
 
-        biomass = biom_arr[bio_indx]
-        if tg_cal == 2: # plot all averages together
-            res_dir = scr_dir + '/' + inp_type + '/'+ biomass +\
-                      '/pdi_' + str(pdi_val) + '/results'
-            if not os.path.isdir(res_dir):
-                os.mkdir(res_dir)
+    # Plot all data
+    indx = 0
+    for t_plt in range(temp_min,temp_max,temp_dt): # loop in temp
+        if yrho_avg[indx] == 0:
+            continue
+            indx += 1
+        sval = 1000.0/yrho_avg[indx]
+        fc_tg.write('%s\t%g\t%g\n' %(biomass,t_plt,sval))
+        indx += 1
 
-            # Write data and then read to concatenate
-            tg_fyl = res_dir + '/tgdata.dat'
-            fc_tg = open(tg_fyl,'w')
-            fc_tg.write('%s\t%s\t%s\n' \
-                        %('Biomass','Temp','SV_NPT'))
-
-
-        tg_avg = np.zeros(0)
-
-        # Define axes labels for distribution
-        fig1,ax1 = plt.subplots()
-        set_axes(ax1,plt,r'Time (ps)',r'Density ($kg$/$m^3$)')
-
-        ytemp_avg = np.zeros(0)
-
-        for tval in range(temp_min,temp_max,temp_dt): # loop in temp
-            temp_leg  = str(tval)
-            yall = np.zeros(0)
-            yavg = 0
-
-            for casenum in range(len(run_arr)): # loop in runarr
-                wdir,tdir,fig_dir = ret_temp_dir(scr_dir,inp_type,biomass,\
-                                                pdi_val,run_arr[casenum],\
-                                                tval,solv_type)
-                print("Analyzing: ", pdi_val,tval,run_arr[casenum])
-                # Check file
-                if os.path.exists(tdir + '/dens_npt.xvg'):
-                    fname  = tdir + '/dens_npt.xvg'
-                elif os.path.exists(wdir + '/dens_npt.xvg'):
-                    fname  = wdir + '/dens_npt.xvg'
-                else:
-                    print(fname, " does not exist! ")
-                    print("ERR: Compute densities before Tg calculation..")
-                    continue
-
-                # Open and parse file
-                with open(fname) as fin:
-                    lines = (line.lstrip() for line in fin \
-                             if not line.lstrip().startswith('#') and \
-                             not line.lstrip().startswith('@'))
-                    data  = np.loadtxt(lines)
-
-                yall = np.append(yall,data[:,1]) #append all y-data
-                startpt = int(0.1*len(yall))
-                yavg += np.average(data[startpt:len(yall)-1,1])
-
-                if casenum == 0 and tval in denarr:
-                    ax1.plot(data[:,0],data[:,1],label=temp_leg)
-
-            # append avg to yavg
-            ytemp_avg = np.append(ytemp_avg,yavg/len(run_arr))
-
-        # Save density plots
-        ax1.legend(loc=0)
-        fig1.savefig(fig_dir + '/'+'dens_'+str(pdi_val)+'.png',\
-                     dpi=fig1.dpi)
-        plt.close(fig1)
-
-        if tg_cal == 2:
-            tval = 300
-            for indx in range(len(ytemp_avg)): 
-                temp_tot  = ytemp_avg[indx]
-                if temp_tot == 0:
-                    continue
-                sval = 1000.0/temp_tot
-                fc_tg.write('%s\t%g\t%g\n' %(biomass,tval,\
-                                             sval))
-                tval += temp_dt
-
-    if tg_cal == 2:
-        fc_tg.close()
-        df=pd.read_table(tg_fyl)
-        figa, axa = plot_tg(df,fig_dir,pdi_val) 
-        tgval = compute_tg(df,axa) #compute tgval
-        figa.savefig(fig_dir + '/'+'sv2_' + str(pdi_val) + '.png',\
-                     dpi=figa.dpi)
-        plt.close(figa)
-
+    fc_tg.close()
+    df=pd.read_table(tg_fyl)
+    figa, axa = plot_tg(df,fig_dir,pdi_val) 
+    tgval = compute_tg(df,axa) #compute tgval
+    figa.savefig(fig_dir + '/'+'sv2_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+    
     if len(pdi_arr) > 1: #plot as a function of PDI
 
         fig2, ax2 = plt.subplots()
-        set_axes(ax2,plt,r'Temperature ($K$)',r'Specific Volume ($cm^3/g$)')
+        set_axes(ax2,plt,r'Temperature ($K$)',r'Specific Volume ($cm^{3}/g$)')
 
         for pdiindx in range(len(pdi_arr)):
             pdiplt = pdi_arr[pdiindx]
