9218596e34ffa3bbb13d739d6dfca1dcf7b2ba38
vaidyanathanms <v0e@ornl.gov>
2022-03-17 17:16:30 -0400

    consolidates hb/rg/rdf/dens/restart

--- myruns/src_py/consolidate_outputs.py
+++ myruns/src_py/consolidate_outputs.py
@@ -1,192 +1,167 @@
 # Version: May-22-2020
 # To consolidate outputs and restart files
 
+#---------------Import Functions-----------------------------------
 import numpy
 import os
 import shutil
 import subprocess
 import sys
 import glob
-
-#---------------Import Functions-----------------------------------
-
 from subprocess import call
-def my_cpy_generic(srcdir,destdir,inpfylname,destfylname):
-    src_fyl  = srcdir  + '/' + inpfylname
-    dest_fyl = destdir + '/' + destfylname
-    shutil.copy2(src_fyl, dest_fyl)
+from plt_aux import *
 
 #---------input flags------------------------------------------
-analysis_type  = 2 #1-polydisp_pe,2-newparams,3-mwchange,#4-oldsize
-rest_job_flag  = 0 #copy restart/job files
+rest_job_flag  = 1 #copy restart/job files
 fyl_flag       = 1 #copy output files
 
 #---------input details----------------------------------------
-run_all   = 1 # 1-copy files and run, 0-NO run (copies files)
 inp_type  = 'melts' # melts, solvents, cosolvents
-biomass   = 'WT' # name of the biomass type
-disp_arr  = [1.8]# dispersity values
-run_arr   = [2,3]  # run number for a given dispersity
-temp_min  = 300  # Minimum temperature
-temp_max  = 321  # Maximum temperature (< max; add +1 to desired)
-temp_dt   = 20   # Temperature dt
-nchains   = 20   # Number of chains - cross check from conf file
-solv_name = 'None' # add this later
-wat_name  = 'None' # add this later
-o_sol_typ = 'None' # add this later
+biom_arr  = ['WT']  # name of the biomass type
+pdi_arr   = [3.0,3.7,'expts'] # dispersity values
+run_arr   = [1,2,3,4]         # run number for a given dispersity
+temp_min  = 250     # Minimum temperature
+temp_max  = 501     # Maximum temperature (< max; add +1 to desired)
+temp_dt   = 10      # Temperature dt
 
 #--------file_lists--------------------------------------------
-#Give prefix for files to be copied followed by *
-fyl_list     = ['adsfrac*','tether*','chainadsval*','log*',\
-                'dens*','chdens*','PErdf*','chgrpdens*','grpdens*'\
-                ,'polydens*','anainp*']
-restart_list = ['restart*','archival*','job*']
+#Give prefix for files to be copied with *
+dir_list     = ['all_hbs','all_radgyr','all_results','all_rdfs']
+fyl_list     = ['*.xvg','*.inp','*.dat','*.ndx']
+restart_list = ['*.cpt','*.log','*.gro']
 
 #---------directory info---------------------------------------
 main_dir  = os.getcwd() # current dir
 gmx_dir   = '/home/v0e/allcodes/files_lignin/pure_melts/myruns/src_gmx' # gmx dir
 tcl_dir   = '/home/v0e/allcodes/files_lignin/pure_melts/myruns/src_tcl' # tcl dir
 sh_dir    = gmx_dir + '/' + 'sh_files' # sh file dir
-scr_dir   = '/lustre/or-scratch/cades-bsd/v0e' # scratch dir
+scr_dir   = '/lustre/or-scratch/cades-bsd/v0e/Glassy_lignin' # scratch dir
 
 if not os.path.isdir(scr_dir):
     print(scr_dir, "does not exist")
     sys.exit()
 #--------------Main Analysis------------------------------------
 
-for disp_val in range(len(disp_arr)): # loop in polydisperse array
-
-    # Make directories
-    head_dir = scr_dir + '/' + inp_type
-    if not os.path.isdir(head_dir):
-        print(head_dir, " does not exist")
+for bio_indx in range(len(biom_arr)): # loop in biomass
+    
+    biomass = biom_arr[bio_indx]
+    headdir = scr_dir + '/' + inp_type + '/'+ biomass
+    if not os.path.isdir(headdir):
+        print("ERR: " + headdir + " not found")
         continue
 
-    poly_dir = head_dir + '/' + biomass
-    if not os.path.isdir(poly_dir):
-        print(poly_dir, " does not exist")
-        continue
+    for pdi_val in pdi_arr:
 
-    poly_dir = poly_dir + '/pdi_' + str(disp_arr[disp_val])
-    if not os.path.isdir(poly_dir):
-        print(poly_dir, " does not exist")
-        continue
+        #--------------Create top-level output directories---------------
+        allop_main = headdir + '/results_pdi_' + str(pdi_val)
+        if not os.path.isdir(allop_main):
+            os.mkdir(allop_main)
 
-    for casenum in range(len(run_arr)): # loop in runarr
+        supp_dir = scr_dir + '/' + inp_type + '/'+ biomass +\
+                   '/restartfiles_pdi_' + str(pdi_val)
+        if not os.path.isdir(supp_dir):
+            os.mkdir(supp_dir)
 
-        rundir = poly_dir + '/run_' + str(run_arr[casenum])
-        if not os.path.isdir(rundir):
-            print(rundir, " does not exist")
-            continue
-        
-        # set main working directory and check input files are present
-        workdir1 = set_working_dir(rundir,inp_type,o_sol_typ)
-        
-        #------------------Make global analysis files output directory-----
-        if fyl_flag == 1:
-
-            out_dir = 'results_PDI_' + str(disp_arr[disp_val])         
-            anafyl_maindir = poly_dir + '/' + out_dir
-            if not os.path.isdir(anafyl_maindir):
-                print("making", anafyl_maindir)
-                os.mkdir(anafyl_maindir)
+      
+        for casenum in range(len(run_arr)): # loop in runarr
+            
+            if pdi_val == 'expts':
+                poly_dir = headdir + '/expts' + '/run_'\
+                           + str(run_arr[casenum])
+            else:
+                poly_dir = headdir + '/pdi_' + str(pdi_val)\
+                           + '/run_' + str(run_arr[casenum])
+            if not os.path.isdir(poly_dir):
+                print(poly_dir + " does not exist!")
+                continue
+            #--------------Make global analysis files output directory-----
+            if fyl_flag == 1:
                 
-            anafyl_casedir = anafyl_maindir + '/run_' + str(run_arr[casenum])
-            if not os.path.isdir(anafyl_casedir):
-                print("making", anafyl_casedir)
-                os.mkdir(anafyl_casedir)
-
-        if rest_job_flag == 1:
-
-            rest_dirname = 'rest_PDI_' + str(disp_arr[disp_val])
-            rest_maindir = poly_dir + '/' + rest_dirname
-            if not os.path.isdir(rest_maindir):
-                os.mkdir(restart_maindir)
-
-            rest_casedir = rest_maindir + '/run_' + str(run_arr[casenum])
-            if not os.path.isdir(rest_casedir):
-                print("making", restart_casedir)
-                os.mkdir(restart_casedir)
-
-        for curr_temp in range(temp_min,temp_max,temp_dt): 
-
-            temp_dir = workdir1 + '/T_'+str(curr_temp)
-            if not os.path.isdir(temp_dir):
-                print(temp_dir, " does not exist")
-                continue
-
-            os.chdir(temp_dir)
-            destdir = os.getcwd()
-            print('Copying: ', biomass,inp_type,'run_'+\
-                  str(run_arr[casenum]), 'T_'+str(curr_temp))
-
-
-            #----Make temp directory in pdi_directory----------
-            if fyl_flag == 1:
-                anafyl_dir = anafyl_casedir + '/T_' + str(curr_temp))
-                if not os.path.isdir(anafyl_dir):
-                    os.mkdir(anafyl_dir)
+                ana_casedir = allop_main + '/run_' + str(run_arr[casenum])
+                if not os.path.isdir(ana_casedir):
+                    print("making", ana_casedir)
+                    os.mkdir(ana_casedir)
+                    
             if rest_job_flag == 1:
-                restart_dir = rest_casedir + '/T_' + str(curr_temp))
-                if not os.path.isdir(restart_dir):
-                    os.mkdir(restart_dir)
 
-            #------All copying/manipulations--------------------------
-            result_dir = 'results_'+ str(free_chains[ifree]) + '_' \
-                         + str(pdi_free) + '_' + str(ncases_pdi[casenum])
-            workdir_results = workdir_subpdi + '/' + result_dir
-            if not os.path.isdir(workdir_results):
-                print(workdir_results, " does not exist")
-                continue
+                rest_casedir = supp_dir + '/run_' + str(run_arr[casenum])
+                if not os.path.isdir(rest_casedir):
+                    print("making", rest_casedir)
+                    os.mkdir(rest_casedir)
 
-            print("Copying output files from", workdir_results)
+            for tval in range(temp_min,temp_max,temp_dt): # loop in temp
 
-            if fyl_flag == 1:
+                wdir,tdir,fig_dir = ret_temp_dir(scr_dir,inp_type,biomass,\
+                                                 pdi_val,run_arr[casenum],\
+                                                 tval,'None')
 
-                for fylcnt in range(len(fyl_list)):
+                if wdir == -1:
+                    print("Temperature directory does not exist: ", tval)
+                    continue
+                os.chdir(wdir)
+                destdir = os.getcwd()
+                print('Copying: ',biomass,inp_type,'run_'+str(run_arr[casenum]),\
+                      'T_'+str(tval))
 
-                    #search in results_directory
-                    os.chdir(workdir_results)
-                    destdir = os.getcwd()
-                    list_of_files = glob.glob(fyl_list[fylcnt])
+                #----Make temp directory in result directory---------------------
+                if fyl_flag == 1: #output results
+                    anafyl_dir = ana_casedir + '/T_' + str(tval)
+                    if not os.path.isdir(anafyl_dir):
+                        os.mkdir(anafyl_dir)
+                if rest_job_flag == 1: #restart backup
+                    restart_dir = rest_casedir + '/T_' + str(tval)
+                    if not os.path.isdir(restart_dir):
+                        os.mkdir(restart_dir)
 
-                    if list_of_files == []: #if list is empty in results dir
+                #------Copying output/restart files ------------------------------
+                if fyl_flag:
 
-                        #search in previous directory
-                        os.chdir(workdir_subpdi)
-                        destdir = os.getcwd()
-                        list_of_files = glob.glob(fyl_list[fylcnt])
-
-                        if list_of_files == []: #if list is empty here too
-                            print("Did not find files of type", \
-                                  fyl_list[fylcnt])
+                    for dirname in dir_list: #sub-directories in output
+                        workdir_results = tdir + '/' + dirname
+                        if not os.path.isdir(workdir_results):
+                            print('Did not find the directory + ', workdir_results)
                             continue
 
-                    print( "Copying files of type: ", fyl_list[fylcnt])
+                        qty_outdir = anafyl_dir + '/' + dirname
+                        if not os.path.isdir(qty_outdir):
+                            os.mkdir(qty_outdir)
 
-                    for filenum in range(len(list_of_files)):
+                        print("Copying output files from", workdir_results)
+                        for fylcnt in range(len(fyl_list)): #search for filetype
+                            # search in subdirectory type
+                            os.chdir(workdir_results)
+                            desdir = os.getcwd()
+                            list_of_files = glob.glob(fyl_list[fylcnt])
 
-                        fylname = list_of_files[filenum]
-                        anafylname = fylname
-                        my_cpy_generic(destdir,anafyl_dir,fylname,anafylname)
+                            if list_of_files == []: #if list is empty
 
-            if rest_job_flag == 1:
+                                print("No filetype: ",fyl_list[fylcnt])
+                                continue
 
-                for fylcnt in range(len(restart_list)):
+                            print( "Copying files of type: ", fyl_list[fylcnt])
 
-                    #search only in main directory
-                    os.chdir(workdir_subpdi)
-                    destdir = os.getcwd()
-                    list_of_files = glob.glob(restart_list[fylcnt])
+                            for filenum in range(len(list_of_files)):
 
-                    if list_of_files == []: #
-                        print("Did not find files of type", \
-                              restart_list[fylcnt])
-                        continue
+                                fylname = list_of_files[filenum]
+                                gencpy(desdir,qty_outdir,fylname)
 
-                    print( "Copying files of type: ", restart_list[fylcnt])
-                    for filenum in range(len(list_of_files)):
+                if rest_job_flag:
 
-                        fylname = list_of_files[filenum]
-                        anafylname = fylname
-                        my_cpy_generic(destdir,restart_dir,fylname,anafylname)# Aux
+                    print("Copying restart files from", tdir)
+                    for fylcnt in range(len(restart_list)):
+
+                        #search only in main directory
+                        os.chdir(tdir)
+                        desdir = os.getcwd()
+                        list_of_files = glob.glob(restart_list[fylcnt])
+
+                        if list_of_files == []: #
+                            print("Did not find files of type", \
+                                  restart_list[fylcnt])
+                            continue
+
+                        print( "Copying files of type: ", restart_list[fylcnt])
+                        for filenum in range(len(list_of_files)):
+
+                            fylname = list_of_files[filenum]
+                            gencpy(desdir,restart_dir,fylname)
