eaf886ff246b460bd104abc88ef1d83fc6471f6d
Vaidyanathan_imac_ORNL <msvaidyanathan1729@gmail.com>
2022-04-25 17:33:02 -0400

    plot defaults

--- myruns/src_matpy/plt_inps.py
+++ myruns/src_matpy/plt_inps.py
@@ -3,6 +3,7 @@
 #Import Modules
 import os
 import matplotlib.pyplot as plt
+#------------------------------------------------------------------
 
 # Directory paths
 main_dir = os.getcwd() # current dir
@@ -18,16 +19,22 @@ if not os.path.isdir(figout_dir):
     os.mkdir(figout_dir)  
 #------------------------------------------------------------------
 
-# Color/line data; figure defaults
+# Color/line data
 orange = '#FFA500'; dark_g = '#006400'; brown = '#8B4513'
-clr_arr = [dark_g,orange,brown,'b','k','m']
-mrk_arr = ['o','d','s']
+clr_arr = [dark_g,orange,brown,'b','k','m','r']
+mrk_arr = ['o','d','s','v','*','H']
 lne_arr = ['-','--']
+#------------------------------------------------------------------
+
+# Default plot data
 plt.rcParams['figure.dpi'] = 1200
-plt.rc('legend',fontsize=16) # fontsize of the legends
+plt.rc('legend',fontsize=12) # fontsize of the legends
 plt.rcParams.update({'font.size': 16}) # other fonts
 plt.rcParams.update({'figure.autolayout': True})
-#plt.rcParams['font.family'] = 'Helvetica'
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Arial']})
+#plt.rcParams['font.family'] = 'Arial' # font type
+plt.rcParams['lines.markersize'] = 10 # marker size
+plt.rcParams['lines.linewidth'] = 2 # line width
 #plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 #plt.rcParams.update({'font.family': 'Times New Roman'})
 #------------------------------------------------------------------
